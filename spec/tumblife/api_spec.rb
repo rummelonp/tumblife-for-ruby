82a2f9c59cf1aaeffc858bdd1b4d01ad7b32d71d
Kazuya Takeshima <mail@mitukiii.jp>
2012-01-08 16:43:39 +0900

    Add request specs to Tumblife::API.

--- spec/tumblife/api_spec.rb
+++ spec/tumblife/api_spec.rb
@@ -3,23 +3,41 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Tumblife::API do
+  def create_response(status, msg)
+    Hashie::Mash.new({
+      body: {
+        meta: {
+          status: status,
+          msg: msg
+        },
+        response: {}
+      }.to_json
+    })
+  end
+
   before do
     @api = Tumblife::API.new
   end
 
-  context :error do
-    def create_response(status, msg)
-      Hashie::Mash.new({
-        body: {
-          meta: {
-            status: status,
-            msg: msg
-          },
-          response: {}
-        }.to_json
-      })
+  context :request do
+    before do
+      @token = @api.access_token
     end
 
+    it 'should get' do
+      @api.should_receive(:access_token).and_return(@token)
+      @token.should_receive(:get).with('/path/to?key1=param1&key2=param2', @api.header).and_return(create_response(200, 'OK'))
+      @api.get('/path/to', key1: 'param1', key2: 'param2')
+    end
+
+    it 'should post' do
+      @api.should_receive(:access_token).and_return(@token)
+      @token.should_receive(:post).with('/path/to', {'key1' => 'param1', 'key2' => 'param2'}, @api.header).and_return(create_response(200, 'OK'))
+      @api.post('/path/to', key1: 'param1', key2: 'param2')
+    end
+  end
+
+  context :error do
     it 'should raise APIError' do
       lambda {
         @api.parse_response(create_response(200, 'OK'))
