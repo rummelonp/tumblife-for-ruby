62fb5101dccbc4e35fa3faea9bf60717e344ef73
Kazuya Takeshima <mail@mitukiii.jp>
2012-01-08 16:01:42 +0900

    Add Tumblife.{client,endpoint} specs.

--- spec/tumblife_spec.rb
+++ spec/tumblife_spec.rb
@@ -1,5 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Tumblife do
+  describe :client do
+    it 'should be a Tumblife::Client' do
+      Tumblife.client.should be_a Tumblife::Client
+    end
+  end
 
+  describe :endpoint do
+    it 'should == Tumblife::Configuration::DEFAULT_ENDPOINT' do
+      Tumblife.endpoint.should == Tumblife::Configuration::DEFAULT_ENDPOINT
+    end
+  end
 end
