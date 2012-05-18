0fc8c8146dc570e264552565bddd15d0a73e5247
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-18 23:41:28 +0900

    Add webmock gem & add webmock helper methods.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,20 @@ require 'tumblife'
 require 'json'
 require 'hashie'
 require 'rspec'
+require 'webmock/rspec'
+
+def a_get(path, endpoint = Tumblife.endpoint)
+  a_request(:get, endpoint + path)
+end
+
+def a_post(path, endpoint = Tumblife.endpoint)
+  a_request(:post, endpoint + path)
+end
+
+def stub_get(path, endpoint = Tumblife.endpoint)
+  stub_request(:get, endpoint + path)
+end
+
+def stub_post(path, endpoint = Tumblife.endpoint)
+  stub_request(:post, endpoint + path)
+end
